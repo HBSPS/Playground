59442f2688dea35f126515f779b108377e6e984a
HBSPS <wiyeong79@naver.com>
2022-11-26 01:16:05 +0900

    :sparkles: [FEAT] 첫번째 눈송이
    눈송이를 위한 keyframes와 스타일 지정

--- src/Routes/Snow.tsx
+++ src/Routes/Snow.tsx
@@ -1,20 +1,46 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 const BackGround = styled.div`
   height: 100vh;
   width: 100vw;
   background: linear-gradient(#123, #111);
+  overflow: hidden;
+  position: relative;
 `;
 
-const Title = styled.h1`
-  color: white;
+const SnowFall = keyframes`
+  0% {
+    transform: translate3d(var(--start), 0, 0);
+  }
+  100% {
+    transform: translate3d(var(--end), 110vh, 0);
+  }
+`;
+
+const SnowFlake = styled.div`
+  --size: 1vw;
+  width: var(--size);
+  height: var(--size);
+  background-color: white;
+  border-radius: 50%;
+  position: absolute;
+  top: -5vh;
+
+  :nth-child(1) {
+    --size: 1vw;
+    --start: -8vw;
+    --end: 3vw;
+    left: 21vw;
+    animation: ${SnowFall} 12s linear infinite;
+    animation-delay: -10s;
+  }
 `;
 
 function Snow() {
   return (
     <>
       <BackGround>
-        <Title>This is Snow!</Title>
+        <SnowFlake />
       </BackGround>
     </>
   );
