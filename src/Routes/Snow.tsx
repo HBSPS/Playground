ea221fc5ab109bce5248c7992e0e445c89287ea3
HBSPS <wiyeong79@naver.com>
2022-11-26 23:58:27 +0900

    :sparkles: [FEAT] 눈송이 스타일 추가

--- src/Routes/Snow.tsx
+++ src/Routes/Snow.tsx
@@ -1,4 +1,5 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
+import SnowFlake from '../Components/Snow/SnowFlakeStyle';
 
 const BackGround = styled.div`
   height: 100vh;
@@ -8,39 +9,12 @@ const BackGround = styled.div`
   position: relative;
 `;
 
-const SnowFall = keyframes`
-  0% {
-    transform: translate3d(var(--start), 0, 0);
-  }
-  100% {
-    transform: translate3d(var(--end), 110vh, 0);
-  }
-`;
-
-const SnowFlake = styled.div`
-  --size: 1vw;
-  width: var(--size);
-  height: var(--size);
-  background-color: white;
-  border-radius: 50%;
-  position: absolute;
-  top: -5vh;
-
-  :nth-child(1) {
-    --size: 1vw;
-    --start: -8vw;
-    --end: 3vw;
-    left: 21vw;
-    animation: ${SnowFall} 12s linear infinite;
-    animation-delay: -10s;
-  }
-`;
-
 function Snow() {
+  const SnowFlakeCount = new Array(50).fill(0);
   return (
     <>
       <BackGround>
-        <SnowFlake />
+        {SnowFlakeCount.map(() => <SnowFlake />)}
       </BackGround>
     </>
   );
